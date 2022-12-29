45457ba8a8b265080d09bb9502f72225c04957f3
Farah <farahhossam121@gmail.com>
2022-12-29 02:25:24 +0200

    edit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-//https://expressjs.com/en/starter/installing.html
-//https://expressjs.com/en/starter/hello-world.html
-//git init
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const bcrypt = require('bcryptjs');
+const userroute = require ("./models/user") 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4002;
 const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use("/api/users" , userroute);
+
 
 app.listen(port, () =>
   console.log(`Server is listening at http://localhost:${port}`)
--- models/user.js
+++ models/user.js
@@ -1,5 +1,18 @@
 var mongoose = require('mongoose');
 
+const router = require("express").Router();
+
+router.get("/usertest",(req, res) => {
+  res.send("user test is successfull")
+});
+
+router.post("userposttest",(req,res) => {
+const username = req.body.username
+res.send("your username is:" +username);
+});
+
+module.exports = router;
+
 const userSchema = new mongoose.Schema({
     name: String,
     password: String,
